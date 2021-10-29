4483a770f65e53e98c83f34281c3bc748069d410
Karel Rosseel <karel.rosseel@gmail.com>
2021-10-29 23:39:30 +0200

    serviceworeker

---
+++ serviceworker.js
@@ -0,0 +1,36 @@
+
+// ZIM Zapps PWA Service Worker to cache app files
+// Please check to see all files have been listed with local links
+// (Do not worry about icon files) 
+
+var cacheName = 'zim_pwa_schrijfhuisje';
+var filesToCache = [
+  './',
+  'schrijfhuisje.html',
+  'libraries/script/>
+',
+  'libraries/script>
+',
+  'libraries/script>',
+  'libraries/script>',
+  'libraries/script>',
+  'libraries/script>'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
